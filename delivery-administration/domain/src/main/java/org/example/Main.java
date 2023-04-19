c772950c0705069e255eb1cfad770c2046584eea
julia-kaminska <kaminska.julia@interia.pl>
2023-04-19 23:49:24 +0200

    Complete version of the project

--- delivery-administration/domain/src/main/java/org/example/Main.java
+++ delivery-administration/domain/src/main/java/org/example/Main.java
@@ -1,7 +1,12 @@
 package org.example;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
+    private static final List<String> SOME_STRINGS = Arrays.asList("Hello", "World");
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        SOME_STRINGS.stream().forEach(System.out::println);
     }
 }
